89f2f4f929d11dcaea6e5162896ec5b1ca7e3b38
kafadare <kafadare@pennmedicine.upenn.edu>
2026-01-27 12:57:07 -0500

    output directory is required arg now, also takes optional centile csv (params_centile) argument now

--- run_rmd.sh
+++ run_rmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=render_rmd
-#SBATCH --output=render_rmd_%j.out
-#SBATCH --error=render_rmd_%j.err
+#SBATCH --output=render_rmd_%x_%j.out
+#SBATCH --error=render_rmd_%x_%j.err
 #SBATCH --time=02:00:00
 #SBATCH --mem=8G
 #SBATCH --cpus-per-task=1
@@ -11,19 +11,20 @@ module load R
 # Rmd file name required
 
 INPUT_RMD=$1
+OUTPUT_DIR=$2
 
-#if no arg supplied, then uses defaults defined inside render.R
-OUTPUT_DIR=${2:-""}
-OUTPUT_BASE=${3:-""}
+#if no arg supplied, then uses defaults defined inside render.R and input script
+PARAMS_CENTILE=${3:-""}
+OUTPUT_BASE=${4:-""}
 
 #conda init
 #conda activate slip_premie
 
 # --- Build command depending on which args are supplied ---
-if [ -n "$OUTPUT_DIR" ] && [ -n "$OUTPUT_BASE" ]; then
-Rscript render.R "$INPUT_RMD" "$OUTPUT_DIR" "$OUTPUT_BASE"
-elif [ -n "$OUTPUT_DIR" ]; then
-Rscript render.R "$INPUT_RMD" "$OUTPUT_DIR"
+if [ -n "$PARAMS_CENTILE" ] && [ -n "$OUTPUT_BASE" ]; then
+Rscript render.R "$INPUT_RMD" "$OUTPUT_DIR" "$PARAMS_CENTILE" "$OUTPUT_BASE"
+elif [ -n "$PARAMS_CENTILE" ]; then
+Rscript render.R "$INPUT_RMD" "$OUTPUT_DIR" "$PARAMS_CENTILE"
 else
-  Rscript render.R "$INPUT_RMD"
+  Rscript render.R "$INPUT_RMD" "$OUTPUT_DIR"
 fi
